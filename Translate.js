dfb05e605cca0cd658a357647d4660ae596183da
Gera Garza <josegerag@hotmail.com>
2020-09-23 22:18:09 -0500

    Add files via upload

--- Translate.js
+++ Translate.js
@@ -1,22 +1,46 @@
 const tr = require("googletrans").default;
-const data = require( './en.json');
+const data = require('./en.json');
+const fs = require('fs')
 
-var spanishObj =data
-
-
-async function translate(lang="en") {
-  for (const property in data) {
-    for (const index in data[property]) {
-      var result = await tr([data[property][index]], { from:"en", to:lang })
-      spanishObj[property][index] = result.text
-    }
-  }
+var languages = {
+  english: "en",
+  spanish: "es",
+  japanese: "ja",
+  korean: "ko",
+  malay: "ms",
+  chinese_simplified: "zh_CN",
+  chinese_traditional: "zh_TW",
+  thai: 'th',
+  indonesian: "id"
 }
 
+var data_copy = data
 
-(async() => {
-  console.log('starting translation...');
-  await translate('es');
-  console.log(JSON.stringify(spanishObj));
-  console.log('finished.');
+async function translate(lang = "en") {
+  for (const property in data) {
+    for (const index in data[property]) {
+      var result = await tr([data[property][index]], {
+        from: "en",
+        to: lang
+      })
+      data_copy[property][index] = result.text
+    }
+  }
+
+  return data_copy
+
+}
+
+(async () => {
+  for (var lang in languages) {
+    console.log(lang + " -> " + languages[lang]);
+
+    var newObj = await translate(languages.spanish)
+
+    fs.writeFile(`${languages[lang]}.json`, JSON.stringify(newObj), e => {
+      if (e) throw e
+    })
+    console.log('finished.');
+
+  }
 })();
